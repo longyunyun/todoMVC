e75977f2f3fecb8788a5331d3b792f472c047822
龙雲 <2210994554@qq.com>
2020-06-25 20:01:00 +0800

    项目结构

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import logo from './logo.svg'
 import './App.css'
-import TodoList from './TodoMVC/TodoList'
+import TodoList from './components/TodoList'
 import Footer from './footer'
 import { Menu } from 'element-react'
 import 'element-theme-default'
