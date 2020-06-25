c8c17d496f97a7a9fc4905f4b05e025d67a5e7b9
龙雲 <2210994554@qq.com>
2020-06-25 15:25:19 +0800

    条件渲染

--- src/TodoMVC/TodoList.js
+++ src/TodoMVC/TodoList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import '../App.css'
 import ListItem from './ListItem'
-import { Button, Card, Checkbox, Layout } from 'element-react'
+import { Button, Card, Checkbox, Layout, Tabs } from 'element-react'
 import 'element-theme-default'
 
 class TodoList extends Component {
@@ -19,6 +19,7 @@ class TodoList extends Component {
       inputVal: '',
     }
   }
+
   addTask () {
     if (!this.state.inputVal) return
     this.setState({
@@ -29,11 +30,13 @@ class TodoList extends Component {
       inputVal: ''
     })
   }
+
   handleChange (e) {
     this.setState({
       inputVal: e.target.value
     })
   }
+
   deleteItem (name) {
     const data = []
     var temp = this.state.notCompleteCount
@@ -50,6 +53,15 @@ class TodoList extends Component {
       notCompleteCount: temp
     })
   }
+
+  deleteCompleteItem () {
+    const data = this.state.list.filter(element => element.status === 0)
+    this.setState({
+      list: data,
+      notCompleteCount: 0
+    })
+  }
+
   completeTask (name) {
     const TodoList = []
     var temp = this.state.notCompleteCount
@@ -73,6 +85,7 @@ class TodoList extends Component {
       }
     })
   }
+
   handleCheckAllChange (checked) {
     const TodoList = []
     this.state.list.forEach((element, index) => {
@@ -100,6 +113,7 @@ class TodoList extends Component {
       isIndeterminate: checkedCount > 0 && checkedCount < citiesLength,
     })
   }
+
   render () {
     return (
       <div className="reactTodoList">
@@ -113,25 +127,39 @@ class TodoList extends Component {
                     <Checkbox
                       checked={this.state.checkAll}
                       indeterminate={this.state.isIndeterminate}
-                      onChange={this.handleCheckAllChange.bind(this)} />
-                    <input type="text" value={this.state.inputVal} onChange={this.handleChange.bind(this)}
+                      onChange={this.handleCheckAllChange.bind(this)} 
+                      />
+                    <input type="text" value={this.state.inputVal} onChange={this.handleChange.bind(this)} 
                       placeholder="在此添加任务"></input>
-                    <span style={{ "float": "right" }}>
+                    <span style={{ "text-align": "right" }}>
                       <Button type="text" icon="plus" onClick={this.addTask.bind(this)} />
                     </span>
                   </span>
                 </div>
               }
             >
-              <div style={{ margin: '15px 0' }}></div>
+              {this.state.list.length > 0 &&
               <div className="text item">
-                <ListItem data={this.state.list} deleteItem={this.deleteItem.bind(this)}
-                  completeTask={this.completeTask.bind(this)} />
+                <Tabs activeName="1">
+                  <Tabs.Pane label={<span>全部任务（{this.state.list.length}）</span>} name="1">
+                    <ListItem data={this.state.list} deleteItem={this.deleteItem.bind(this)}
+                      completeTask={this.completeTask.bind(this)} />
+                  </Tabs.Pane>
+                  <Tabs.Pane label={<span>已完成（{this.state.notCompleteCount}）</span>} name="2">
+                    <ListItem data={this.state.list.filter(element => element.status === 1)} deleteItem={this.deleteItem.bind(this)}
+                      completeTask={this.completeTask.bind(this)} />
+                  </Tabs.Pane>
+                  <Tabs.Pane label={<span>待完成（{this.state.list.length - this.state.notCompleteCount}）</span>} name="3">
+                    <ListItem data={this.state.list.filter(element => element.status === 0)} deleteItem={this.deleteItem.bind(this)}
+                      completeTask={this.completeTask.bind(this)} />
+                  </Tabs.Pane>
+                </Tabs>
+                <span style={{ "float": "right" }}>
+                  {this.state.list.filter(element => element.status === 1).length > 0 &&
+                    <Button type="text" icon="delete" onClick={this.deleteCompleteItem.bind(this)} >删除已完成项目</Button>}
+                </span>
               </div>
-              <div >
-                <p style={{ "font": "12px  Extra ,Extra ,Small" }}>共{this.state.list.length}个任务,已完成{this.state.notCompleteCount}个,
-                {this.state.list.length - this.state.notCompleteCount}个待完成</p>
-              </div>
+            }
             </Card>
           </Layout.Col>
         </Layout.Row>
