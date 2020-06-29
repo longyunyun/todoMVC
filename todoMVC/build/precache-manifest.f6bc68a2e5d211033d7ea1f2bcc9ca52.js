4f339d56c193d22ad466d6908841548f27eadd2c
龙雲 <2210994554@qq.com>
2020-06-29 23:03:15 +0800

    build Dockerfile

---
+++ todoMVC/build/precache-manifest.f6bc68a2e5d211033d7ea1f2bcc9ca52.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14d4e9f3446f4fc0c8148af02eda0ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1104559f275d30c508e6",
+    "url": "./static/css/2.a77f9b8c.chunk.css"
+  },
+  {
+    "revision": "6dcfe686fe6856623332",
+    "url": "./static/css/main.813bfa67.chunk.css"
+  },
+  {
+    "revision": "1104559f275d30c508e6",
+    "url": "./static/js/2.fe5e786d.chunk.js"
+  },
+  {
+    "revision": "2f97bfe5b7c1c72b23be28739730a643",
+    "url": "./static/js/2.fe5e786d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dcfe686fe6856623332",
+    "url": "./static/js/main.a1928634.chunk.js"
+  },
+  {
+    "revision": "b75f52007c9d1f47f2cd",
+    "url": "./static/js/runtime-main.1cc0be6c.js"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "./static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "./static/media/element-icons.d2f69a92.woff"
+  }
+]);
